cb9422c3d5c2d618351f1184e162e4bfd88a5649
Anoob Bava <abava@enableconsulting.com>
2022-04-15 14:16:47 +0530

    Added devise for authentication

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# create a user with a password
+user = User.create!(email: 'admin@admin.com', password: 'password', password_confirmation: 'password')
