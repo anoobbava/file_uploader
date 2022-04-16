e0f8de26ec2798b56e21db93a5a28f4e76af84f0
Anoob Bava <abava@enableconsulting.com>
2022-04-16 07:23:57 +0530

    Integrated bootstrap to the application

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,13 @@
 const { environment } = require('@rails/webpacker')
+const webpack = require('webpack')
+
+environment.plugins.append('Provide', new webpack.ProvidePlugin({
+  $: 'jquery',
+  jQuery: 'jquery',
+  // uncomment below for bootstrap 4.x
+  // Popper: ['popper.js', 'default']
+  // uncomment below for bootstrap 5
+  Popper: ['@popperjs/core', 'default']
+}))
 
 module.exports = environment
